02a8d2c111a32342d7d185fc0c412abf0e6dbe82
Sascha Kattelmann <skattelmann@gmail.com>
2014-04-25 12:30:26 +0200

    working

--- pip200644.h
+++ pip200644.h
@@ -1,16 +1,18 @@
 #include <vector>
 #include <math.h> 
+#include <gmpxx.h>
+#define PREC 512
 
 namespace N200644 {
 
 class Point {
 private:
-    long double _x, _y;
+    mpf_class _x, _y;
 public:
     Point() { _x = 0; _y = 0; }
-    Point(double x, double y) { _x = (long double) x; _y = (long double) y; }
-    long double x() const {return _x;}
-    long double y() const {return _y;}
+    Point(double x, double y) {  _x = x; _y = y; _x.set_prec (PREC); _y.set_prec (PREC); }
+    mpf_class x() const {return _x;}
+    mpf_class y() const {return _y;}
 };
 
 template <typename InputIterator>
