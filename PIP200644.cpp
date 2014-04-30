d6b106f6aa102e1f7f0edbf23d122151adfc7949
Sascha Kattelmann <skattelmann@gmail.com>
2014-04-30 17:58:32 +0200

    holy shit

--- PIP200644.cpp
+++ PIP200644.cpp
@@ -28,16 +28,16 @@ bool Polygon::contains( const Point& q ) const {
     if ( wn == 0 ){
         for( int i=0; i < size; ++i ){
 
-            /* case: upward egde -> increment winding number */
-            if (points[i].x() <= q.x()) {
-                if (points[i+1].x() > q.x())
+            /* case: downward egde -> increment winding number */
+            if (points[i].y() >= q.y()) {
+                if (points[i+1].y() < q.y())
                     if ( checkSide(q, points[i], points[i+1]) > 0 )
                         ++wn;
             }
 
-            /* case: downward egde -> decrement winding number */
+            /* case: upward egde -> decrement winding number */
             else { 
-                if (points[i+1].x() <= q.x())
+                if (points[i+1].y() >= q.y())
                     if ( checkSide(q, points[i], points[i+1]) < 0 )
                         --wn;
             }
