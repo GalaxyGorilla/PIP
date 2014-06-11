02837ce698532bc4ad216a6560e54abd83f3ca9c
Sascha Kattelmann <skattelmann@gmail.com>
2014-06-11 15:28:40 +0200

    fine tuning

--- src/PIP200644.cpp
+++ src/PIP200644.cpp
@@ -16,18 +16,19 @@ bool Polygon::contains( const Point& q ) const {
     int wn = 0, i;
     /* line to the right */
     for( i=0; i < size; ++i ){
-        if (points[i].getx() == q.getx()
-            && points[i].gety() == q.gety())
+        if( points[i].getx() == q.getx()
+            && points[i].gety() == q.gety() )
             return 1;
         
         /* case: upward egde -> increment winding number */
-        if (points[i].gety() <= q.gety()) {
-            if (points[i+1].gety() > q.gety()
-                && checkSide(q, points[i], points[i+1]) > 0 )
+        if( points[i].gety() <= q.gety() ){
+            if( points[i+1].gety() > q.gety() ){
+                if( checkSide(q, points[i], points[i+1]) > 0 )
                 ++wn;
+            }
         }
         /* case: downward egde -> decrement winding number */
-        else if (points[i+1].gety() <= q.gety()
+        else if( points[i+1].gety() <= q.gety()
             && checkSide(q, points[i], points[i+1]) < 0 )
             --wn;
     }
@@ -35,17 +36,17 @@ bool Polygon::contains( const Point& q ) const {
     /* 
      * after the first iteration it is only possible to return a false negative result,
      * to avoid this the same procedure with other reference lines is repeated;
-     * these reference lines should cover every possible special case 
      */
 
     if ( wn == 0 ) /* line to the left */
     for( i=0; i < size; ++i ){
-        if (points[i].gety() >= q.gety()) {
-            if (points[i+1].gety() < q.gety()
-                && checkSide(q, points[i], points[i+1]) > 0 )
+        if( points[i].gety() >= q.gety() ){
+            if( points[i+1].gety() < q.gety() ){
+                if( checkSide(q, points[i], points[i+1]) > 0 )
                 ++wn;
+            }
         }
-        else if (points[i+1].gety() >= q.gety()
+        else if( points[i+1].gety() >= q.gety()
             && checkSide(q, points[i], points[i+1]) < 0 )
             --wn;
     }
